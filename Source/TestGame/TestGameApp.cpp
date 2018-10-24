4f1de9bfe071c22242bdfd7d6d07a863dbb0f12c
Bear <tbearcuf@gmail.com>
2018-10-24 14:24:49 -0700

    Added testgamelogic and instance creation.

--- Source/TestGame/TestGameApp.cpp
+++ Source/TestGame/TestGameApp.cpp
@@ -1,4 +1,5 @@
 #include "TestGameApp.h"
+#include "TestGameLogic.h"
 
 TestGameApp::TestGameApp(int argc, char *argv[]) : GameEngineApp(argc, argv)
 {
@@ -7,5 +8,6 @@ TestGameApp::TestGameApp(int argc, char *argv[]) : GameEngineApp(argc, argv)
 
 BaseGameLogic *TestGameApp::VCreateGameAndView()
 {
+    m_pGame = new TestGameLogic();
     return m_pGame;
 }
---
+++ Source/TestGame/TestGameLogic.cpp
@@ -0,0 +1,6 @@
+#include "TestGameLogic.h"
+
+TestGameLogic::TestGameLogic()
+{
+
+}
