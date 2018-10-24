09f84af04085fe8cf2661969cf05ff2ad32633ce
Bear <tbearcuf@gmail.com>
2018-10-24 14:41:25 -0700

    Call BaseGameLogic initialization from VCreateGameAndView

--- Source/TestGame/TestGameApp.cpp
+++ Source/TestGame/TestGameApp.cpp
@@ -9,5 +9,11 @@ TestGameApp::TestGameApp(int argc, char *argv[]) : GameEngineApp(argc, argv)
 BaseGameLogic *TestGameApp::VCreateGameAndView()
 {
     m_pGame = new TestGameLogic();
+    //initialize the game logic
+    m_pGame->Init();
+
+//    shared_ptr<IGameView> menuView(GCC_NEW MainMenuView());
+//	m_pGame->VAddView(menuView);
+
     return m_pGame;
 }
